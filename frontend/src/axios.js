6c0512ac79627937428fe5b218ae3040d40c4138
ranadeep <viranadeep@gmail.com>
2025-07-05 22:37:51 +0530

    Merge_Links_error

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 // src/axios.js
 import axios from "axios";
 
-const BASE_URL = import.meta.env.NODE_ENV==="development"? "http://localhost:5000/api" : "/api";
+const BASE_URL = import.meta.env.MODE==="development"? "http://localhost:5000/api" : "/api";
 const api = axios.create({
   baseURL: BASE_URL,
   withCredentials: true, // This allows cookies to be sent with requests
